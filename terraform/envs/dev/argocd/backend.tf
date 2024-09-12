650bbf4503d82777bd0a36f93ac7aefac9bd17a8
Muhammad-Imtiaz <imtiaz9832@gmail.com>
2024-09-12 10:28:10 +0500

    Add argocd caller module in dev environment

---
+++ terraform/envs/dev/argocd/backend.tf
@@ -0,0 +1,12 @@
+# TERRAFORM BACKEND
+terraform {
+  backend "s3" {
+    bucket  = "digitify-terraform-state-remote-backend"
+    key     = "dev/argocd/terraform.tfstate"
+    region  = "us-east-1"
+
+    # ENABLES STATE LOCKING
+    dynamodb_table = "digitify-terraform-state-lock"
+
+  }
+}
